18ec9b30831a1823d05ad85ffb19844e2d72355b
PietGil <pieter-gillis@hotmail.com>
2018-11-05 18:20:20 +0100

    'first'

---
+++ config.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+
+
+author Maarten Slembrouck <maarten.slembrouck@ugent.com>
+
+uncomment and fill in the right data
+
+*/
+#tekd
+
+$servername = "localhost";
+$username = "project8";
+$password = "project8";
+$dbname = "project8";
+
+?>
